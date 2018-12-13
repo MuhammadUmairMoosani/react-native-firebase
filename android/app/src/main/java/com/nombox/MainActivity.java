5a28b048f6f7d1664582b6d53a44aaa50155b6c5
muhammadumairmoosani <muhammadumairmoosani@gmail.com>
2018-12-13 19:03:39 +0500

    react native integrated react-native-firebase

---
+++ android/app/src/main/java/com/nombox/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nombox;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Nombox";
+    }
+}
